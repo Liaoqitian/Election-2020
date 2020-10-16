cf1cc75d2017a77f277a6e57a635a66ab1cf1d4f
Qitian Liao <liaoqitian1024@gmail.com>
2020-10-16 00:51:34 -0400

    ballotview done

--- app/src/main/java/com/example/represent/BallotView.java
+++ app/src/main/java/com/example/represent/BallotView.java
@@ -2,6 +2,7 @@ package com.example.represent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -21,7 +22,11 @@ public class BallotView extends AppCompatActivity {
     static String API_KEY = "AIzaSyDugNQO9vZxbi68BQnReZCd_CeM-cg-WW0";
     static String VOTE_URL = "https://www.googleapis.com/civicinfo/v2/voterinfo";
 
-    private TextView ballotLocationTv, pollingLocationTv, dropOffLocationTv;
+    private TextView ballotLocationTv;
+    private TextView electionEventTv;
+    private TextView pollingLocationTv;
+    private TextView dropOffLocationTv;
+    private TextView ballotMeasuresTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,42 +36,72 @@ public class BallotView extends AppCompatActivity {
         String location = getIntent().getExtras().getString("location");
         String address = getIntent().getExtras().getString("address");
         ballotLocationTv = findViewById(R.id.ballotLocation);
+        electionEventTv = findViewById(R.id.electionEvent);
         pollingLocationTv = findViewById(R.id.pollingLocation);
         dropOffLocationTv = findViewById(R.id.dropOffLocation);
-
+        ballotMeasuresTv = findViewById(R.id.ballotMeasures);
         if (address != null) printInformation(address);
-
+        else pollingLocationTv.setText("Invalid Address");
 
         ballotLocationTv.setText(location);
-        pollingLocationTv.setText("hello");
     }
 
     private void printInformation(String address) {
         RequestQueue queue = Volley.newRequestQueue(this);
-        String place_URL = "?address=" + address.replace("\\s+", "");
+        String place_URL = "?address=" + address;
         String full_URL = VOTE_URL + place_URL + "&key=" + API_KEY;
         JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.GET, full_URL, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                JSONArray pollingLocations;
-                String locationName, line1, pollingLocation;
+                JSONArray pollingLocations, dropOffLocations;
+                String electionEvent;
+                String pollingLocationName, pollingLine1, pollingLocation;
+                String dropOffLocationName, dropOffLine1, dropOffLocation;
+                String ballotMeasure;
+                try {
+                    electionEvent = response.getJSONObject("election").getString("name");
+                    electionEventTv.setText(electionEvent);
+                } catch (JSONException e) {
+                    electionEventTv.setText("N/A");
+                    e.printStackTrace();
+                }
                 try {
                     pollingLocations = response.getJSONArray("pollingLocations");
-                    locationName = pollingLocations.getJSONObject(0).getJSONObject("address").getString("locationName");
-                    line1 = pollingLocations.getJSONObject(0).getJSONObject("address").getString("line1");
-                    pollingLocation = locationName + ", " + line1;
-                    pollingLocationTv.setText("entered");
-
+                    pollingLocationName = pollingLocations.getJSONObject(0).getJSONObject("address").getString("locationName");
+                    pollingLine1 = pollingLocations.getJSONObject(0).getJSONObject("address").getString("line1");
+                    pollingLocation = pollingLocationName + ", " + pollingLine1;
+                    pollingLocationTv.setText(pollingLocation);
                 } catch (JSONException e) {
+                    pollingLocationTv.setText("N/A");
+                    e.printStackTrace();
+                }
+                try {
+                    dropOffLocations = response.getJSONArray("dropOffLocations");
+                    dropOffLocationName = dropOffLocations.getJSONObject(0).getJSONObject("address").getString("locationName");
+                    dropOffLine1 = dropOffLocations.getJSONObject(0).getJSONObject("address").getString("line1");
+                    dropOffLocation = dropOffLocationName + ", " + dropOffLine1;
+                    dropOffLocationTv.setText(dropOffLocation);
+                } catch (JSONException e) {
+                    dropOffLocationTv.setText("N/A");
+                    e.printStackTrace();
+                }
+                try {
+                    ballotMeasure = response.getJSONArray("contests").getJSONObject(0).getString("ballotTitle");
+                    ballotMeasuresTv.setText(ballotMeasure);
+                } catch (JSONException e) {
+                    ballotMeasuresTv.setText("N/A");
                     e.printStackTrace();
                 }
             }
         }, new Response.ErrorListener() {
-
             @Override
             public void onErrorResponse(VolleyError error) {
-                pollingLocationTv.setText("There is an error");
+                electionEventTv.setText("N/A");
+                pollingLocationTv.setText("N/A");
+                dropOffLocationTv.setText("N/A");
+                ballotMeasuresTv.setText("N/A");
             }
         });
+        queue.add(stringRequest);
     }
 }
